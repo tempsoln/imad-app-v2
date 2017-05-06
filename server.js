ecbe06f74f0ee531584f4fa9862aa646b821a5fe
tempsoln <akhil.venkateswaran@gmail.com>
2017-05-06 15:17:43 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var bodyparser = require('body-parser');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var session = require('express-session');
 var app = express();
+
 app.use(morgan('combined'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended : true}));
+app.use(session({
+    
+    secret : 'vamefa00',
+    cookie : {maxage : 30*24*60*60*1000},
+    resave : false,
+    saveUninitialized : false
+    
+}));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+var config = {
+    user: 'mbvhjkuddonhoa',
+    database: 'db3fj52a60edal',
+    host: 'ec2-54-163-254-76.compute-1.amazonaws.com',
+    port: '5432',
+    password: '4c792d9037905c571b8043c4015009015b6345ffbea7d28e41371843d5dda21f'
+};
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+var pool=new Pool(config);
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+
+var port = process.env.PORT || 8000;
+
+app.listen(port , function(){
+    
+   console.log("Server listening at port " + port);
+    
 });
